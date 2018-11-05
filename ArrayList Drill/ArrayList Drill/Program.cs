0264599e17007243e69f40977eb5ece309e7349d
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-05 12:39:07 -0600

    Add files via upload

--- ArrayList Drill/ArrayList Drill/Program.cs
+++ ArrayList Drill/ArrayList Drill/Program.cs
@@ -15,60 +15,79 @@ namespace ArrayList_Drill
 
             //STRING ARRAY
 
-            string[] array1 = { "A", "B", "C", "D", "E" };
+            string[] array1 = { "Chicago Bears", "Detroit Lions", "Greenbay Packers", "Minnesota Vikings" };
 
-            Console.WriteLine("Find the index of the entered value.  Enter a letter between A and E.  (Only enter a capital letter.");
+            Console.WriteLine("Enter a number between 0 and 3 to find out what NFL team is in the NFC North division:");
             string value = Console.ReadLine();
-            int position = Array.IndexOf(array1, value);
 
-            if (position > 0 || position <= 4)
+            if (Int32.TryParse(value, out int resultIndex1))
             {
-                Console.WriteLine("The index of the entered value is {0}:", position);
+                if (resultIndex1 >= 0 && resultIndex1 < array1.Length)
+                {
+                    string valueAtIndex = array1[resultIndex1];
+                    Console.WriteLine("The {0} are in the NFC North division.", valueAtIndex);
+                }
+                else
+                {
+                    Console.WriteLine("The value entered is not between 0 and 3.");
+                }
             }
-            else 
+            else
             {
                 Console.WriteLine("What's wrong with you? Enter a valid value.");
             }
 
             Console.ReadLine();
 
+
             //INT ARRAY
-            int[] array2 = { 4, 1, 3, 2 };
+            int[] array2 = { 1991, 1993, 1994, 1996, 1998 };
 
-            Console.WriteLine("Choose a number between 1 and 4 to find the index of the array:");
-            string input = Console.ReadLine();
-            Int32.TryParse(input, out inputValue1);
-            int numPosition = Array.IndexOf(array2, inputValue1);
+            Console.WriteLine("Enter a number between 0 and 4 to find out which year in the 90's Pearl Jam released a new album:");
+            string valueTwo = Console.ReadLine();
+            int Num;
 
-            if (numPosition == 0 || numPosition == 1 || numPosition == 2 || numPosition == 3)//finds index
+            if (Int32.TryParse(valueTwo, out Num))
             {
-                Console.WriteLine("You chose index {0}.", numPosition);
-            }
-            else if (!Int32.TryParse(input, out inputValue1))//anything other than an integer
-            {
-                Console.WriteLine("C'mon, man!  Enter a valid value.");
+                if (Num >= 0 && Num < array2.Length)
+                {
+                    int numAtIndex = array2[Num];
+                    Console.WriteLine("Pearl Jam released a new record in {0}.", numAtIndex);
+                }
+                else
+                {
+                    Console.WriteLine("The value entered is not between 0 and 4.");
+                }
             }
             else
             {
-                Console.WriteLine("Dude, that number ain't even an index.");
+                Console.WriteLine("What's wrong with you? Enter a valid value.");
             }
+
             Console.ReadLine();
 
 
             //LIST OF STRINGS
-            List<string> myList = new List<string>() { "A", "B", "C", "D" };
+            List<string> myList = new List<string>() { "Snickers", "100 Grand", "Butterfinger", "Milky Way", "Baby Ruth", "Heath"  };
 
-            Console.WriteLine("Choose a letter A, B, C, or D to find the index of the list:");
+            Console.WriteLine("Enter a number betwen 0 and 5 to find a candy bar in the list:");
             string listValue = Console.ReadLine();
-            int index = myList.IndexOf(listValue);
 
-            if (index > 0 || index <= 3)
+            if (Int32.TryParse(listValue, out int listIndex))
             {
-                Console.WriteLine("You chose index {0}.", index);
+                if (listIndex >= 0 && listIndex < myList.Count)
+                {
+                    string listValueAtIndex = myList[listIndex];
+                    Console.WriteLine("The {0} candy bar is in the list.", listValueAtIndex);
+                }
+                else
+                {
+                    Console.WriteLine("The value entered is not between 0 and 5.");
+                }
             }
             else
             {
-                Console.WriteLine("That number ain't even an index.");
+                Console.WriteLine("What's wrong with you? Enter a valid value.");
             }
 
             Console.ReadLine();
