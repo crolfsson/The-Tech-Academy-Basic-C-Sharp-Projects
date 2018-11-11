06cd7141545810b1ba1cd0d11cd74dcc90bbd67f
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-11 15:53:35 -0600

    Add files via upload

---
+++ ClassMethod3/ClassMethod3/MathOp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod3
+{
+    public class Calculator
+    {
+        public double Number { get; set; }
+
+        public double Add(double number)
+        {
+            return number + 1;
+        }
+
+        public double Square(double number)
+        {
+            return number * number;
+        }
+
+        public double Zero(double number)
+        {
+            return number * 0;
+        }
+
+        internal double Add(int v, object num)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
---
+++ ClassMethod3/ClassMethod3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Enter number for math operation: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(calculator.Add(number));
+            Console.WriteLine(calculator.Square(number));
+            Console.WriteLine(calculator.Zero(number));
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for your entry.  Have a nice day!");
+            Console.ReadLine();
+        }
+    }
+}
