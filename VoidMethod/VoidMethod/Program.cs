e3b4ef71b761a2309eb92f0d37bf6133de7d2294
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-13 16:04:58 -0600

    Add files via upload

---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Void Method:  Return 2nd Integer");
+            Console.ReadKey();
+
+            TwoParm twoparm = new TwoParm();
+
+            twoparm.Add(2,25);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ VoidMethod/VoidMethod/TwoParm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class TwoParm
+    {
+        //public double oneNumber { get; set; }
+        public double result { get; set; }
+
+        public void Add(int oneNumber, int twoNumber)
+        {
+            double result;
+            result = oneNumber + twoNumber;
+            Console.WriteLine("The second integer is {0}.", twoNumber);
+        }
+    }
+}
