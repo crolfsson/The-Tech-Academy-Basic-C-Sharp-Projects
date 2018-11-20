0039e59b59d0643c1df6408c0e8b5007d3f86cd0
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-19 19:31:56 -0600

    Add files via upload

--- ThreeMethodMath/ThreeMethodMath/Program.cs
+++ ThreeMethodMath/ThreeMethodMath/Program.cs
@@ -8,26 +8,22 @@ namespace ThreeMethodMath
 {
     class Program
     {
-        private static string resultOne;
-        private static string resultTwo;
-        private static string resultThree;
-        private static int methodResult;
-
-        //private static int resultTwo;
-
         static void Main(string[] args)
         {
+            Console.WriteLine("Three INT Method Math");
+            Console.ReadLine();
+
             Calc calculator = new Calc();
 
             int result1 = calculator.basic(5);
             Console.WriteLine("The sum of the integer value minus 3 equals: " + result1);
             Console.ReadLine();
 
-            double result2 = calculator.basic(3.2);
+            int result2 = calculator.basic(3.2);
             Console.WriteLine("The rounded result of the double value multiplied by 4.2 equals: " + result2);
             Console.ReadLine();
 
-            string result3 = calculator.basic(25);
+            int result3 = calculator.basic("25");
             Console.WriteLine("The string value plus 1 equals: " + result3);
             Console.ReadLine();
 
