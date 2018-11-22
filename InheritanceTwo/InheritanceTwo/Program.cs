9fb4e30169c0869d76cdc878e3b2af14aae3cb10
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-22 09:57:05 -0600

    Add files via upload

---
+++ InheritanceTwo/InheritanceTwo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance Two Drill");
+            Console.WriteLine("\n");
+
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
