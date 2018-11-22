320a89ef906921966fc74f37e00317952ae92ec9
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-22 08:22:20 -0600

    Add files via upload

---
+++ InheritanceOne/InheritanceOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceOne
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance One Drill");
+            Console.WriteLine("\n");
+
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
