91f0c2ad623101d185b8d5011ffe7ff22d7ebd54
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-24 11:43:14 -0600

    Add files via upload

---
+++ OverloadOperatorOne/OverloadOperatorOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperatorOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Overload Operator One Drill");
+            Console.WriteLine("\n");
+
+            //COMMENTING OUT NAME CODE - NOT APPLICABLE FOR THIS DRILL
+            //Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //employee.SayName();
+            //Console.ReadLine();
+
+            Employee employeeId1 = new Employee(12345);
+            Employee employeeId2 = new Employee(1234);
+
+            if (employeeId1 == employeeId2)
+                Console.WriteLine("Both employees have the same Employee ID.");
+            else
+                Console.WriteLine("Each employee has a unique Employee ID.");
+            Console.ReadLine();
+        }
+
+        static void Quit(IQuittable employee)
+        {
+            employee.Quit();
+        }
+    }
+}
