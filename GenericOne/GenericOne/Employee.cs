5242560bc108be88be23aeb682e928410d9c55dc
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-26 16:17:47 -0600

    Add files via upload

---
+++ GenericOne/GenericOne/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericOne
+{
+    public class Employee<T> : Person, IQuittable
+    {
+        //GENERIC EXAMPLE
+        public List<T> Things { get; set; }
+
+        public void AddThings(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ForEach(Action writeLine)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //ID PROPERTY AND BOOLEAN LOGIC FOR EQUAL IDS - NOT APPLICABLE FOR THIS DRILL
+        //int employeeId;
+        //public Employee()
+        //{
+        //    employeeId = 0;
+        //}
+        //public Employee(int x)
+        //{
+        //    employeeId = x;
+        //}
+        //public static bool operator == (Employee x, Employee y)
+        //{
+        //    if (x.employeeId == y.employeeId)
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        //public static bool operator != (Employee x, Employee y)
+        //{
+        //    if (x.employeeId != y.employeeId)
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        //public int getValue()
+        //{
+        //    return employeeId;
+        //}
+
+        //PERSON CLASS
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        //IQUITTABLE INTERFACE
+        public void Quit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GenericOne/GenericOne/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generic One Drill");
+            Console.WriteLine("\n");
+
+            //GENERIC EXAMPLE
+            Employee<string> stringEmployee = new Employee<string>();
+            stringEmployee.AddThings("Business Analyst");
+            stringEmployee.AddThings("Software Developer");
+            stringEmployee.AddThings("Technical Lead");
+
+            Employee<int> intEmployee = new Employee<int>();
+            intEmployee.AddThings(1);
+            intEmployee.AddThings(2);
+            intEmployee.AddThings(3);
+
+            stringEmployee.ForEach(Console.WriteLine);
+            intEmployee.ForEach(Console.WriteLine);
+
+            Console.ReadLine();
+
+
+            //COMMENTING OUT NAME CODE - NOT APPLICABLE FOR THIS DRILL
+            //Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //employee.SayName();
+            //Console.ReadLine();
+
+            //COMMENTING OUT OVERLOAD OPERATOR CODE - NOT APPLICABLE FOR THIS DRILL
+            //Employee employeeId1 = new Employee(12345);
+            //Employee employeeId2 = new Employee(1234);
+
+            //if (employeeId1 == employeeId2)
+            //    Console.WriteLine("Both employees have the same Employee ID.");
+            //else
+            //    Console.WriteLine("Each employee has a unique Employee ID.");
+            //Console.ReadLine();
+        }
+
+        static void Quit(IQuittable employee)
+        {
+            employee.Quit();
+        }
+    }
+}
