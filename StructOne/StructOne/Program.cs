ca4837c7e68139e7d1f51d6d99f8323e4b119786
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-27 20:26:54 -0600

    Add files via upload

---
+++ StructOne/StructOne/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Struct Drill");
+            Console.WriteLine("\n");
+
+
+            Number value = new Number();
+            value.Amount = 7;
+            Console.WriteLine("The assigned value is {0}.", value.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
