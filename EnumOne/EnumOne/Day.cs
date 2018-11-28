f6213c8e69d2a4408362e2a2e5262b2e7127f82f
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-11-27 19:48:28 -0600

    Add files via upload

---
+++ EnumOne/EnumOne/Day.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumOne
+{
+    public class Week
+    {
+        public Day Day { get; set; }
+    }
+
+    public enum Day
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ EnumOne/EnumOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumOne
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enum Drill:  Days of the Week");
+            Console.WriteLine("\n");
+
+            Week Week = new Week();
+
+            Console.WriteLine("Please enter the current day of the week.");
+            Day actualDay = Day.Tuesday;
+
+            try
+            {
+                actualDay = (Day)Enum.Parse(typeof(Day), Console.ReadLine());
+                Console.WriteLine("{0} is an awesome day of the week!", actualDay);
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
