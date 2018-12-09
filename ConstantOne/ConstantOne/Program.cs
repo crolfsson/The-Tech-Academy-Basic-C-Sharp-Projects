97b5f128824c995cabccb35f7d7e707d16bfe1c4
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-12-08 19:14:13 -0600

    Add files via upload

---
+++ ConstantOne/ConstantOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string school = "Newport Elementary School";
+
+            Console.WriteLine("Constant One Drill");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Welcome new students to {0}!", school);
+            Console.Write("Please enter you first name:  ");
+            string studentFirstName = Console.ReadLine();
+            Console.Write("Please enter you last name:  ");
+            string studentLastName = Console.ReadLine();
+
+            var newStudent = new Student(studentFirstName, studentLastName);
+            Console.WriteLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("{0} wishes you a great school year!", school);
+            Console.ReadLine();
+            
+
+
+
+
+        }
+    }
+}
---
+++ ConstantOne/ConstantOne/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantOne
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Student()
+        {
+            Console.Write("Welcome, ");
+        }
+
+        public Student(string firstName): this()
+        {
+            Console.Write(firstName);
+        }
+
+        public Student(string firstName, string lastName) : this ("new student") 
+        {
+            Console.Write(" " + firstName + " " + lastName +"!");
+        }
+
+        //public Student(string name) : this(name, 99999)//New students get a pseudo ID of 99999 to distinguish them as new
+        //{
+        //    //No parameter is needed.
+        //}
+    }
+}
