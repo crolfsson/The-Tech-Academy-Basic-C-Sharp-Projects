a587d98c29b084129d1efacbfd54e527ac2e6ce7
crolfsson <43209265+crolfsson@users.noreply.github.com>
2018-12-09 10:28:04 -0600

    Add files via upload

---
+++ ExceptionOne/ExceptionOne/AgeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionOne
+{
+    public class AgeException : Exception
+    {
+        public AgeException()//inheriting from the base excpetion
+            : base() { }
+
+        public AgeException(string message)//overloading from the first constructor
+            : base(message) { }
+    }
+}
---
+++ ExceptionOne/ExceptionOne/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionOne
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exception One Drill");
+            Console.WriteLine("\n");
+
+
+            Console.Write("Please enter your age:  ");
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                int birthYear = DateTime.Now.Year - (age);
+                if (age > 0)
+                {
+                    Console.WriteLine("Wow!  You were born in {0}.  You are aging gracefully!", birthYear);
+                }
+                else
+                {
+                    throw new AgeException();
+                }
+
+            }
+            catch (AgeException)
+            {
+                Console.WriteLine("Please enter a value greater than zero.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sytem Error.  Please enter a valid value.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
